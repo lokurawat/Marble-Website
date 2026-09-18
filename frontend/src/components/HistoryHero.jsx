3eb9835a2d8d55c4da4db23ca286df3dbf6c262d
lokurawat <rawatlokesh960@gmail.com>
2026-09-18 20:04:03 +0530

    update some components

--- frontend/src/components/HistoryHero.jsx
+++ frontend/src/components/HistoryHero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HistoryHero = ({ heading, paragraph, reverse }) => {
+const HistoryHero = ({ heading, paragraph, reverse, img }) => {
 
   return (
 
@@ -16,8 +16,8 @@ const HistoryHero = ({ heading, paragraph, reverse }) => {
 
         <img
           className='w-full h-[300px] sm:h-[500px] lg:h-[90vh] object-cover rounded'
-          src="https://imgs.search.brave.com/k0gU0_Volkb-hdMCbRJoZoSmMrooqDyoympanjaB3wI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRpYS5nZXR0eWltYWdl/cy5jb20vaWQvMTE5/MTg4NDQ5OC9waG90/by9hZHVsdC1tYW4t/d29ya2luZy1vbi1w/cm9kdWN0aW9uLWxp/bmUtaW4tc3RvbmUt/cHJvY2Vzc2luZy1m/YWN0b3J5LmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1QcjJV/MVRoaFIzRkJMZzUw/R2dyandZWHlLTG5t/T2xvSE8zM1FzYWg2/eGNNPQ"
-          alt=""
+          src={img}
+          alt={heading}
         />
 
       </div>
