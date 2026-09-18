a19d1fd775a06ef04291d441b7ff94df2e79ce0e
lokurawat <rawatlokesh960@gmail.com>
2026-09-19 01:53:03 +0530

    made all changes

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,12 +2,24 @@ import React, { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 
 const Button = (props) => {
-const {addToCart}=useContext(CartContext)
-  return (
-    <button onClick={(e)=>{
-      e.preventDefault()
+
+  const { addToCart } = useContext(CartContext)
+
+  const handleClick = (e) => {
+
+    e.preventDefault()
+
+    if (props.item) {
       addToCart(props.item)
-    }} className='bg-green-500 px-5 py-2 rounded cursor-pointer text-white active:scale-95 transition-all duration-150'>
+    }
+
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className='bg-green-500 px-5 py-2 rounded cursor-pointer text-white active:scale-95 transition-all duration-150'
+    >
       {props.button}
     </button>
   )
