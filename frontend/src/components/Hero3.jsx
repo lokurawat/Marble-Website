adf1c26c896ce7e7b0b851e460f5de9171897f65
lokurawat <rawatlokesh960@gmail.com>
2026-09-19 01:42:08 +0530

    made all changes

--- frontend/src/components/Hero3.jsx
+++ frontend/src/components/Hero3.jsx
@@ -27,7 +27,12 @@ const Hero3 = () => {
 
         </p>
 
-         <Link to={"/getAllProducts"}>  <div className='bg-green-500 px-5 py-2 rounded cursor-pointer text-white active:scale-95 transition-all duration-150'>Shop Now</div>  </Link>
+        <Link
+  to="/getAllProducts"
+  className="bg-green-500 px-5 py-2 rounded cursor-pointer text-white active:scale-95 transition-all duration-150"
+>
+  Shop Now
+</Link>
 
       </div>
 
